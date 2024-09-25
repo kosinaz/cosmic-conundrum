1bd78cde90955fc56d097ad7161e5d31f88d61ae
Kosina Zoltán <kosinaz@gmail.com>
2024-09-25 12:09:49 +0200

    add clues

--- Cosmic Conundrum/ingame.gd
+++ Cosmic Conundrum/ingame.gd
@@ -8,7 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"%Card".get_node("%MainSign").texture = load("res://assets/6.png")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
